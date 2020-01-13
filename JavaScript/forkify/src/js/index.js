c4ecc5f0c26f218aac35436da4579f8dc3ad2e8f
kkoomin <kkooming2@gmail.com>
2020-01-13 17:06:23 +0900

    node js setup

--- JavaScript/forkify/src/js/index.js
+++ JavaScript/forkify/src/js/index.js
@@ -10,6 +10,7 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  * - Liked recipes
  */
 
+// state = to store all the data in one central place
 const state = {};
 
 /**
@@ -28,12 +29,17 @@ const controlSearch = async () => {
     searchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4) Search for recipes
-    await state.search.getResults();
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
 
-    // 5 render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+      // 5 render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      alert("Somehthing wrong with search...");
+      clearLoader();
+    }
   }
 };
 
@@ -66,17 +72,27 @@ const controlRecipe = async () => {
     // Create new recipe object
     state.recipe = new Recipe(id);
 
-    // Get recipe data
-    await state.recipe.getRecipe();
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
 
-    // Calculate servings and time
-    state.recipe.calcTime();
-    state.recipe.calcServings();
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
 
-    // Render recipe
-    console.log(state.recipe);
+      // Render recipe
+      console.log(state.recipe);
+    } catch (err) {
+      alert("Error processing recipe!");
+    }
   }
 };
 
-//hashchange event
-window.addEventListener("hashchange", controlRecipe);
+// To execute same event listener in two different event
+["hashchange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
+// =>
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+// load => when the page loads
